320bf8a81685deb79a81baed8d7ae069682179b2
иван кедров <ivkedrov@gmail.com>
2022-12-19 21:49:36 +0300

    Added new HW

---
+++ conftest.py
@@ -0,0 +1,27 @@
+'''
+Создайте GitHub-репозиторий, в котором будут лежать файлы conftest.py и test_items.py.
+Добавьте в файл conftest.py обработчик, который считывает из командной строки параметр language.
+Реализуйте в файле conftest.py логику запуска браузера с указанным языком пользователя. Браузер должен объявляться в фикстуре browser и передаваться в тест как параметр.
+В файл test_items.py напишите тест, который проверяет, что страница товара на сайте содержит кнопку добавления в корзину. Например, можно проверять товар, доступный по http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/.
+Тест должен запускаться с параметром language следующей командой:
+pytest --language=es test_items.py
+и проходить успешно. Достаточно, чтобы код работал только для браузера Сhrome.
+'''
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action = 'store', default = None, help = 'Choose language')
+
+@pytest.fixture(scope='function')
+def browser(request):
+    user_language = request.config.getoption('language')
+    print("\nstart browser for test..")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options = options)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
