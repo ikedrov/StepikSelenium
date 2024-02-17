0ecefd430cae6c5564a75972ca5033eb0b9f9180
Ivan <ivkedrov@gmail.com>
2024-02-17 21:33:46 +0300

    preparing for POM

--- 3.2.2.py
+++
@@ -1,9 +0,0 @@
-'''
-Вам дан шаблон для функции test_substring, которая принимает два значения: full_string и substring.
-
-Функция должна проверить вхождение строки substring в строку full_string с помощью оператора assert и, в случае несовпадения, предоставить исчерпывающее сообщение об ошибке.
-'''
-
-def test_substring(full_string, substring):
-    # ваша реализация, напишите assert и сообщение об ошибке
-    assert substring in full_string, f"expected '{substring}' to be substring of '{full_string}'"
--- conftest.py
+++ conftest.py
@@ -1,4 +1,4 @@
-'''
+"""
 Создайте GitHub-репозиторий, в котором будут лежать файлы conftest.py и test_items.py.
 Добавьте в файл conftest.py обработчик, который считывает из командной строки параметр language.
 Реализуйте в файле conftest.py логику запуска браузера с указанным языком пользователя. Браузер должен объявляться в фикстуре browser и передаваться в тест как параметр.
@@ -6,22 +6,24 @@
 Тест должен запускаться с параметром language следующей командой:
 pytest --language=es test_items.py
 и проходить успешно. Достаточно, чтобы код работал только для браузера Сhrome.
-'''
+"""
 
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-def pytest_addoption(parser):
-    parser.addoption('--language', action = 'store', default = None, help = 'Choose language')
 
-@pytest.fixture(scope='function')
+def pytest_addoption(parser):
+    parser.addoption("--language", action="store", default=None, help="Choose language")
+
+
+@pytest.fixture(scope="function")
 def browser(request):
-    user_language = request.config.getoption('language')
+    user_language = request.config.getoption("language")
     print("\nstart browser for test..")
     options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-    browser = webdriver.Chrome(options = options)
+    options.add_experimental_option("prefs", {"intl.accept_languages": user_language})
+    browser = webdriver.Chrome(options=options)
     yield browser
     print("\nquit browser..")
     browser.quit()
